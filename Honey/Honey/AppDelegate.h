d438ea9c042a6ec9feede05fb83c09ab9eea66ea
zhemdaisy <zhemdaisy@gmail.com>
2013-11-21 22:40:24 +0800

    init project

---
+++ Honey/Honey/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Honey
+//
+//  Created by Daisy on 13-11-21.
+//  Copyright (c) 2013年 Daisy&Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
