a3a551c529fbd1b133f0cb79cc72f4ab7f25f27f
zhemdaisy <zhemdaisy@gmail.com>
2013-11-24 20:53:56 +0800

    用下xcode 提交代码

--- Honey/Honey/音效/SoundPlayManager.h
+++ Honey/Honey/音效/SoundPlayManager.h
@@ -7,7 +7,23 @@
 //
 
 #import <Foundation/Foundation.h>
+#include <AudioToolbox/AudioToolbox.h>
 
-@interface SoundsPlayManger : NSObject
+@interface SoundPlayManager : NSObject{
+    CFURLRef		soundFileURLRef;
+    SystemSoundID	soundFileObject;
+}
+@property (readwrite)	CFURLRef		soundFileURLRef;
+@property (readonly)	SystemSoundID	soundFileObject;
 
+/**
+ * 初始化单例
+ **/
++ (SoundPlayManager*)sharedInstance;
+
+/**
+ * @brief 根据提供的音频播放音效
+ *
+ */
+- (void)playSoundWithName:(NSString*)name;
 @end
--- Honey/Honey/音效/SoundPlayManager.m
+++ Honey/Honey/音效/SoundPlayManager.m
@@ -6,8 +6,47 @@
 //  Copyright (c) 2013年 Daisy&Jack. All rights reserved.
 //
 
-#import "SoundsPlayManger.h"
+#import "SoundPlayManager.h"
 
-@implementation SoundsPlayManger
+
+
+@implementation SoundPlayManager
+
+@synthesize soundFileObject;
+@synthesize soundFileURLRef;
+
+
+static SoundPlayManager *soundPlayManagerObj = nil;
+- (id)init{
+    @synchronized(self){
+        self = [super init];
+        
+        return self;
+    }
+    return nil;
+}
++ (SoundPlayManager*)sharedInstance{
+    static SoundPlayManager *sharedSoundPlayManager = nil;
+    static dispatch_once_t predicate;
+    dispatch_once(&predicate, ^{
+        sharedSoundPlayManager = [[self alloc]init];
+    });
+    return nil;
+}
+
+- (void)playSoundWithName:(NSString *)name{
+    NSURL *tapSound   = [[NSBundle mainBundle] URLForResource: name
+                                                withExtension: @"aif"];
+    
+    // Store the URL as a CFURLRef instance
+    self.soundFileURLRef = (__bridge CFURLRef) tapSound ;
+    
+    // Create a system sound object representing the sound file.
+    AudioServicesCreateSystemSoundID (
+                                      soundFileURLRef,
+                                      &soundFileObject
+                                      );
+    AudioServicesPlaySystemSound (soundFileObject);
+}
 
 @end
